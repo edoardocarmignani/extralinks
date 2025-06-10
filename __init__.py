092fc184857898b02014bbce72e54bef1159ded1
Edoardo Carmignani <edoardo.carmignani@gmail.com>
2025-06-10 12:54:47 +0200

    Refractoring

--- __init__.py
+++ __init__.py
@@ -6,11 +6,11 @@
 #     "WEB_DIRECTORY",
 # ]
 
-__author__ = """Edoardo Carmignani"""
-__email__ = "edoardo.carmignani@gmail.com"
-__version__ = "0.0.1"
+__author__ = 'Edoardo Carmignani'
+__email__ = 'edoardo.carmignani@gmail.com'
+__version__ = '0.0.1'
 
-WEB_DIRECTORY = "./web"
+WEB_DIRECTORY = './web'
 
 NODE_CLASS_MAPPINGS = {}
 NODE_DISPLAY_NAME_MAPPINGS = {}
